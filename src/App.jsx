688dcafea4d8f54523c0dd45367a36fda096e0cd
Suhail Razi P M <suhailrazij@gmail.com>
2023-10-29 16:47:55 +0530

    FIle name error

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
 import Portfolio from './components/portfolio/Portfolio';
-import Services from './components/services/services';
+import Services from './components/services/Services';
 
 const App = () => {
   return <div>
